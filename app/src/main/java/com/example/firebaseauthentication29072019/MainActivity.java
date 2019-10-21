2befcd38e58d57ceab058e3b54f57805204d6728
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-10-21 19:03:02 +0700

    'A'

---
+++ app/src/main/java/com/example/firebaseauthentication29072019/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.firebaseauthentication29072019;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEdtEmail, mEdtPassword;
+    Button btnDangky, btnDangnhap, btnThongTinUser, btnXacthucEmail;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEdtEmail = findViewById(R.id.edittextEmail);
+        mEdtPassword = findViewById(R.id.edittextPassword);
+        btnDangky = findViewById(R.id.buttonDangky);
+        btnDangnhap = findViewById(R.id.buttonDangnhap);
+        btnThongTinUser = findViewById(R.id.buttonThongtin);
+        btnXacthucEmail = findViewById(R.id.buttonVerification);
+        btnDangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEdtEmail.getText().toString();
+                String password = mEdtPassword.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Log.d("BBB", task.getException().toString());
+                                }
+                            }
+                        });
+            }
+        });
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEdtEmail.getText().toString();
+                String password = mEdtPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Log.d("BBB", task.getException().toString());
+                                }
+                            }
+                        });
+            }
+        });
+        btnThongTinUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getIdToken() instead.
+                    String uid = user.getUid();
+                    Toast.makeText(MainActivity.this, String.format("Name : %s " + "\n" +
+                            "Email : %s " + "\n" +
+                            "photoUrl : %s " + "\n" +
+                            "uid : %s ", name, email, photoUrl, uid), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnXacthucEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Đã gửi xác thực thành công", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.d("BBB", task.getException().toString());
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
